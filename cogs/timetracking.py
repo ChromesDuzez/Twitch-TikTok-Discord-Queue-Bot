02c4c9d206b085ff3c6a5da37e9584799c999385
Zach Wilson <68037788+ChromesDuzez@users.noreply.github.com>
2024-05-27 16:19:15 -0500

    createClock method, and minor changes
    - Major update for `createclock()` method where it now store's the time clock message in the db under employee... now just need to update the content of the message.
    - made minor data validation changes throughout the code and general tidying up.
    - specified fields in sqlite `INSERT` query in `addemployee()` method
    - made changes to the tables in `verifyDatabase()` (now `dbSetup()`) including...
         - the removal of the `customer` table for simplicity's sake
         - added multiple new fields for employees for pay calculation report and storing time clock for the bot
         - added new `employee_type` table with some default values.

--- cogs/timetracking.py
+++ cogs/timetracking.py
@@ -3,16 +3,50 @@ from discord.ext import commands
 import sqlite3
 import os
 
+class Confirm(discord.ui.View):
+    def __init__(self, user: discord.User):
+        super().__init__()
+        self.user = user
+        self.value = None
+
+    @discord.ui.button(label="Yes", style=discord.ButtonStyle.green)
+    async def confirm(self, button: discord.ui.Button, interaction: discord.Interaction):
+        if interaction.user != self.user:
+            await interaction.response.send_message("This is not for you!", ephemeral=True)
+            return
+        self.value = True
+        self.stop()
+        await interaction.response.send_message("You clicked Yes!", ephemeral=True)
+
+    @discord.ui.button(label="No", style=discord.ButtonStyle.red)
+    async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
+        if interaction.user != self.user:
+            await interaction.response.send_message("This is not for you!", ephemeral=True)
+            return
+        self.value = False
+        self.stop()
+        await interaction.response.send_message("You clicked No!", ephemeral=True)
+
+
 
 class TimeTracking(commands.Cog): # create a class for our cog that inherits from commands.Cog
     # this class is used to create a cog, which is a module that can be added to the bot
 
     def __init__(self, bot): # this is a special method that is called when the cog is loaded
-        self.db = os.getcwd() + "\\timetracker.db"
-        self.verifyDatabase(self.db)
+        self.cwd = os.getcwd()
+        self.db = self.cwd + "\\timetracker.db"
+        self.dbSetup(self.db)
         self.bot = bot
 
+                    ## Employee Type Methods
+        ### Less important methods ###
+    #add type(name, rate, id=-1) -  admin command
+    #edit type(id, name="", rate="") -  admin command
+    #remove type(id) - admin command
 
+
+                    ## Employee Methods
+    # add employee(name, phonenumber, addressline1, city, state, zip, addressline2="",user="") - admin command   *updating needed
     @discord.slash_command(name="addemployee", description="Add a new Employee to from discord to the system.")
     @commands.has_permissions(administrator=True)
     async def addemployee(self, 
@@ -32,7 +66,7 @@ class TimeTracking(commands.Cog): # create a class for our cog that inherits fro
         valid = True
         if user is not None:
             try:
-                newid = int(user[2:-1:])
+                newid = int(user.strip()[2:-1:])
                 id = newid
             except:
                 valid = False
@@ -48,31 +82,153 @@ class TimeTracking(commands.Cog): # create a class for our cog that inherits fro
         if valid:
             value = [(id, name, phonenumber, addressline1, addressline2, city, state, zip)]
             try:
-                cursor.executemany('INSERT INTO employee VALUES (?, ?, ?, ?, ?, ?, ?, ?)', value)
+                cursor.executemany('INSERT INTO employee (id, name, phoneNumber, addressLine1, addressLine2, addressCity, addressState, addressZip) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', value)
                 conn.commit()
                 conn.close()
                 await ctx.respond(f"Added new employee {value[0][1]} (aka <@{value[0][0]}>)")
                 print(f"Added new employee {value[0][1]} (aka <@{value[0][0]}>)")
             except:
                 await ctx.respond(f"Error adding new employee {value[0][1]} (aka <@{value[0][0]}>)")
-                print(f"Error adding new employee {value[0][1]} (aka <@{value[0][0]}>)")
-    
-
+                print(f"Error adding new employee {value[0][1]} (aka <@{value[0][0]}>) by {ctx.author}")
     # Error handling for missing permissions
     @addemployee.error
     async def addemployee_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
-            await ctx.respond(f"{ctx.author}, you do not have the necessary permissions to use this command.")
+            await ctx.respond(f"{ctx.author}, you do not have the necessary permissions to use this command.", ephemeral=True)
             print(f"{ctx.author}, you do not have the necessary permissions to use this command.")
     
+    # create clock(user, channelid="") - admin command
+    @discord.slash_command(name="createclock", description="Create a time clock embed for a user.")
+    @commands.has_permissions(administrator=True)
+    async def createclock(self, 
+                          ctx: discord.ApplicationContext,
+                          user: discord.Option(str, description="The discord user you wish to make a time clock for."),   # type: ignore
+                          channel: discord.Option(str, default=None, description="Choose a different channel to create the time clock.")   # type: ignore
+                          ):
+        ## check that the values given were in the correct format
+        try:
+            _ = int(user[2:-1:])
+            if channel is not None:
+                _ = int(user[2:-1:])
+        except:
+            if channel is None:
+                await ctx.respond(f"The parameters submitted were not in the correct format for {user}: {user[2:-1:]}", ephemeral=True)
+            else:
+                await ctx.respond(f"The parameters submitted were not in the correct format for {user}: {user[2:-1:]} or {channel[2:-1:]}", ephemeral=True)
+            return
+        # connect to db
+        conn = sqlite3.connect(self.db)
+        cursor = conn.cursor()
+        ## use the data in the db to check that we aren't overwriting good data
+        cursor.execute(f"SELECT clockChannelId, clockMessageId FROM employee WHERE id = {user[2:-1:]}")
+        employees = cursor.fetchall()
+        if len(employees) == 0:
+            await ctx.respond(f"Cannot edit employee {user} because they don't exist in the database.", ephemeral=True)
+            print(f"Cannot edit employee {user} because they don't exist in the database.")
+            conn.commit()
+            conn.close()
+            return
+        elif len(employees) > 1:
+            await ctx.respond(f"There is a significant bug in the code that <@336231886198276096> needs to look into.", ephemeral=True)
+            print("How did we get multiple employees back with the same id?!!!!!")
+            conn.commit()
+            conn.close()
+            return
+        elif employees[0][1] is not None:
+            view = Confirm(user=ctx.user)
+            await ctx.response.send_message("This emplyee already has a time clock, do you want to proceed?", view=view, ephemeral=True)
+            await view.wait()
+            if view.value:
+                oldMessageID = employees[0][1]
+                oldChannelID = employees[0][0]
+                print(f"Override Confirmed by {view.user}... Trying to delete old time clock message.")
+                try:
+                    # Fetch the channel by ID
+                    valid = True
+                    delchannel = self.bot.get_channel(oldChannelID)
+                    if delchannel is None:
+                        print(f"Channel with ID {oldChannelID} not found.")
+                        valid = False
+                    # Fetch the message by ID
+                    if valid:
+                        message = await delchannel.fetch_message(oldMessageID)
+                        # Delete the message
+                        await message.delete()
+                        print(f"Message with ID {oldMessageID} deleted from channel {oldChannelID}.")
+                except discord.NotFound:
+                    print("Message or channel not found.")
+                except discord.Forbidden:
+                    print("I do not have permission to delete this message.")
+                except discord.HTTPException as e:
+                    print(f"Failed to delete message: {e}")
+            else:
+                await ctx.followup.send("You chose not to proceed or did not respond in time.", ephemeral=True)
+                return
+        ## go to channel and send the message
+        channelObj = ctx.channel
+        if channel is not None:
+            channelObj = self.bot.get_channel(int(channel[2:-1:]))
+        message = await channelObj.send("content")
+        ## store the message id and channel id in the db
+        cursor.execute(f"UPDATE employee SET clockChannelId = {message.channel.id}, clockMessageId = {message.id} WHERE id = {user[2:-1:]}")
+        ## wrap it all up with a nice clean bow
+        conn.commit()
+        conn.close()
+        await ctx.respond(f"Clock created successfully for {user}", ephemeral=True)
+        print(f"{ctx.author} - Clock created successfully for {user}")
+
+        
+    # delete clock(user) - admin command
+
+
+        ### Less important methods ###
+    #edit employee(user, name="", phonenumber="", addressline1="", addressline2="", city="", state="",, zip="") - admin command
+    #remove employee(user) - admin command
+
+
+                ## Punch Clock Methods
+    # display punches(user="")
+
+    # punch in(user="")
+
+    # punch out(user="")
+
+    # update clock(user) - private method 
+
+        ### Less important methods ###
+    #edit punch(id) - admin command - trigger via button?
+
+
+                ## Work Time Methods
+    # display work time(user)
+
+    #add work time(customer, punchtype, timespent, punchid=-1)
+
+        ### Less important methods ###
+    #edit work time(id, punchid=-1, customer="", punchtype="", timespent=0) - admin perms to edit someone else's - trigger via button?
+    #remove work time(id) - admin command? - trigger via button?
+
 
     ## Database setup if it doesn't already exist
-    def verifyDatabase(self, db):
+    def dbSetup(self, db):
         if not os.path.exists(db):
             f = open(db, "w")
             f.close()
             conn = sqlite3.connect(db)
             cursor = conn.cursor()
+            #sets up employee type table
+            cursor.execute('''
+                CREATE TABLE employee_type (
+	                id             INTEGER            PRIMARY KEY,
+	                name           TEXT               NOT NULL,
+	                rate           DECIMAL(10,5)      NOT NULL
+                )
+            ''')
+            employeeTypesDefaultData = [
+                ('Clerical', 1.5),
+                ('Construction', 1.7)
+            ]
+            cursor.executemany('INSERT INTO employee_type (name, rate) VALUES (?, ?)', employeeTypesDefaultData)
             #sets up employee table
             cursor.execute('''
                 CREATE TABLE employee (
@@ -83,7 +239,12 @@ class TimeTracking(commands.Cog): # create a class for our cog that inherits fro
                     addressLine2   TEXT                NOT NULL DEFAULT '',
                     addressCity    TEXT                NOT NULL,
                     addressState   TEXT                NOT NULL,
-                    addressZip     TEXT                NOT NULL
+                    addressZip     TEXT                NOT NULL,
+	                payrate        DECIMAL(10,2)       NOT NULL DEFAULT 16.00,
+	                employeeTypeID INTEGER             NOT NULL DEFAULT 2,
+                    clockChannelId UNSIGNED BIG INT    NULL DEFAULT NULL,
+                    clockMessageId UNSIGNED BIG INT    NULL DEFAULT NULL,
+                    FOREIGN KEY (employeeTypeID) REFERENCES employee_type(id)
                 )
             ''')
             #sets up punch clock table
@@ -96,29 +257,15 @@ class TimeTracking(commands.Cog): # create a class for our cog that inherits fro
                     FOREIGN KEY (employeeID) REFERENCES employee(id)
                 )
             ''')
-            #sets up customer table
-            cursor.execute('''
-                CREATE TABLE customer (
-                    id             UNSIGNED BIG INT    PRIMARY KEY,
-                    name           TEXT                NOT NULL,
-                    phoneNumber    TEXT                NULL DEFAULT NULL,
-                    addressLine1   TEXT                NULL DEFAULT NULL,
-                    addressLine2   TEXT                NULL DEFAULT NULL,
-                    addressCity    TEXT                NULL DEFAULT NULL,
-                    addressState   TEXT                NULL DEFAULT NULL,
-                    addressZip     TEXT                NULL DEFAULT NULL
-                )
-            ''')
             #sets up work time table
             cursor.execute('''
-                CREATE TABLE users (
-                    id          UNSIGNED BIG INT                                      PRIMARY KEY,
-                    punchID     UNSIGNED BIG INT                                      NOT NULL,
-                    customerID  UNSIGNED BIG INT                                      NOT NULL,
-                    punchType   TEXT CHECK( punchType IN ('POOL','SERVICE') )         NOT NULL,
-                    timeSpent   INTEGER CHECK( timeSpent > 0 AND timeSpent <= 1440)   NOT NULL,
-                    FOREIGN KEY (punchID) REFERENCES punch_clock(id),
-                    FOREIGN KEY (customerID) REFERENCES customer(id)
+                CREATE TABLE work_time (
+                    id          UNSIGNED BIG INT                                                             PRIMARY KEY,
+                    punchID     UNSIGNED BIG INT                                                             NOT NULL,
+                    customer    TEXT                                                                         NOT NULL,
+                    punchType   TEXT CHECK( punchType IN ('POOL','SERVICE') )                                NOT NULL,
+                    timeSpent   INTEGER CHECK( timeSpent > 0 AND timeSpent <= 1440 AND timeSpent % 15 = 0)   NOT NULL,
+                    FOREIGN KEY (punchID) REFERENCES punch_clock(id)
                 )
             ''')
             conn.commit()
