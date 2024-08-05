25c3a62caa3ed78195d9bf884a813cfbe7243da0
Zach Wilson <68037788+ChromesDuzez@users.noreply.github.com>
2024-08-04 23:41:44 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "pip install py-cord"
+pip install py-cord
+echo "pip install xlsxwriter"
+pip install xlsxwriter
+echo "pip install openpyxl"
+pip install openpyxl
+echo "pip install python-dotenv"
+pip install python-dotenv
+
+read -p "Press enter to continue..." -n 1 -s 
+
+python main.py
+
+read -p "Press enter to continue..." -n 1 -s 
