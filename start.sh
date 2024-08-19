425b39a451ce8a240181daf598c04fe0ab9da148
Zach Wilson <68037788+ChromesDuzez@users.noreply.github.com>
2024-08-19 18:52:10 -0500

    Update start.sh
    I am going to be removing xlwings from the entire project because it doesnt run on linux

--- start.sh
+++ start.sh
@@ -3,8 +3,6 @@ echo "pip install py-cord"
 pip install py-cord
 echo "pip install xlsxwriter"
 pip install xlsxwriter
-echo "pip install xlwings"
-pip install xlwings
 echo "pip install openpyxl"
 pip install openpyxl
 echo "pip install python-dotenv"
@@ -12,6 +10,6 @@ pip install python-dotenv
 
 read -p "Press enter to continue..." -n 1 -s 
 
-python main.py
+python3 main.py
 
 read -p "Press enter to continue..." -n 1 -s 
