e6956555e5ac5571effe4bb7a414404d854ff5c7
Zach Wilson <68037788+ChromesDuzez@users.noreply.github.com>
2024-11-03 22:01:34 -0600

    Update start.sh
    Fixed to installer

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "pip install py-cord"
-pip install py-cord --break-system-packages
+pip3 install py-cord 
 echo "pip install xlsxwriter"
-pip install xlsxwriter --break-system-packages
+pip3 install xlsxwriter
 echo "pip install openpyxl"
-pip install openpyxl --break-system-packages
+pip3 install openpyxl
 echo "pip install python-dotenv"
-pip install python-dotenv --break-system-packages
+pip3 install python-dotenv
 
 read -p "Press enter to continue..." -n 1 -s 
 
