d61641266e42fee0fb71574054e11e91c96564c9
Zach Wilson <68037788+ChromesDuzez@users.noreply.github.com>
2024-11-03 21:13:45 -0600

    Update start.sh
    Damn .sh file needs to actually do the pip install. It w/o this change it does check for the packages properly but doesnt install properly via cli.

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "pip install py-cord"
-pip install py-cord
+pip install py-cord --break-system-packages
 echo "pip install xlsxwriter"
-pip install xlsxwriter
+pip install xlsxwriter --break-system-packages
 echo "pip install openpyxl"
-pip install openpyxl
+pip install openpyxl --break-system-packages
 echo "pip install python-dotenv"
-pip install python-dotenv
+pip install python-dotenv --break-system-packages
 
 read -p "Press enter to continue..." -n 1 -s 
 
