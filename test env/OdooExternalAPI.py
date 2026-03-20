5bbdabf69fd10723008b0e52bb3d89f1187ed408
Zach Wilson <68037788+ChromesDuzez@users.noreply.github.com>
2026-03-20 18:24:37 -0500

    First Fetched Data
    Now time to test editing and creating data on the database

--- test env/OdooExternalAPI.py
+++ test env/OdooExternalAPI.py
@@ -1,4 +1,46 @@
-url = 'see .env'
-db = 'see .env'
-username = 'see .env'
-password = 'see .env'
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv() 
+
+OdooLoaded = False
+OdooURL = os.getenv("ODOO_URL", None)
+OdooDB = os.getenv("ODOO_DB", None)
+OdooUSERNAME = os.getenv("ODOO_USERNAME", None)
+OdooKEY = os.getenv("ODOO_API_KEY", None)
+
+if not all([OdooURL, OdooDB, OdooUSERNAME, OdooKEY]):
+    print("Odoo configuration is incomplete. Please check your .env file if you wish to use the Odoo integration.")
+else:
+    OdooLoaded = True
+    print("Odoo configuration loaded successfully.")
+
+
+## Example of making a request to Odoo API to fetch partners whose display name starts with 'a'
+if OdooLoaded:
+    response = requests.post(
+        f"{OdooURL}/res.partner/search_read",
+        headers={
+            "Authorization": f"Bearer {OdooKEY}",
+            "X-Odoo-Database": f"{OdooDB}"
+        },
+        json={
+            "context": {"lang": "en_US"},
+            "domain": [
+                [
+                    "id",
+                    "=",
+                    1
+                ]
+            ],
+            "fields": [
+                "id",
+                "display_name"
+            ],
+            "limit": 10
+        },
+    )
+    response.raise_for_status()
+    data = response.json()
+    print(data)
