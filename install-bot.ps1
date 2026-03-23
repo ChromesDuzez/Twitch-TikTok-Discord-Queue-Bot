0aeef519ea1bbbe9e315cc5803fa1a41af595f3c
Zach Wilson <68037788+ChromesDuzez@users.noreply.github.com>
2026-03-23 16:46:14 -0500

    need to force python version 3.12

--- install-bot.ps1
+++ install-bot.ps1
@@ -1,8 +1,10 @@
 Write-Host "Setting up the virtual environment..."
-python -m venv .venv
+py -3.12 -m venv .venv
 Write-Host "Virtual environment created. Starting virtual environment..."
 .venv\Scripts\activate
 Write-Host "Installing required packages..."
 pip install -r requirements.txt
+Write-Host "Packages installed. Upgrading pip..."
+py -m pip install --upgrade pip
 Write-Host "All packages installed. Deactivating the virtual environment..."
 deactivate
