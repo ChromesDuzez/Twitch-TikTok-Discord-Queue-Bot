ad244f194c748627ebc78b0f520dab71ad23fb94
Zach Wilson <68037788+ChromesDuzez@users.noreply.github.com>
2026-03-23 16:54:51 -0500

    Update install-bot.sh
    forced python version 3.12 on both macos, linux and windows now with the install-bot.sh and .ps1 scripts

--- install-bot.sh
+++ install-bot.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 echo "Setting up the virtual environment..."
-python3 -m venv .venv
+python3.12 -m venv .venv
 echo "Virtual environment created. Starting virtual environment..."
 source .venv/bin/activate
 echo "Installing required packages..."
 pip install -r requirements.txt
+echo "Packages installed. Upgrading pip..."
+pip install --upgrade pip
 echo "All packages installed. Deactivating the virtual environment..."
 deactivate
