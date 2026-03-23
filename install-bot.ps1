07b42b2a7d035de95ec935b5da350cd4989a38a4
Zach Wilson <68037788+ChromesDuzez@users.noreply.github.com>
2026-03-23 15:38:54 -0500

    moved required packages into a requirements file
    that way i only need to update the requirements for the project in one place rather than both install scripts

--- install-bot.ps1
+++ install-bot.ps1
@@ -3,13 +3,6 @@ python -m venv .venv
 Write-Host "Virtual environment created. Starting virtual environment..."
 .venv\Scripts\activate
 Write-Host "Installing required packages..."
-pip install py-cord
-pip install xlsxwriter
-pip install xlwings
-pip install openpyxl
-pip install python-dotenv
-pip install xlwings
-pip install prompt_toolkit
-pip install requests
+pip install -r requirements.txt
 Write-Host "All packages installed. Deactivating the virtual environment..."
 deactivate
--- install-bot.sh
+++ install-bot.sh
@@ -4,13 +4,6 @@ python3 -m venv .venv
 echo "Virtual environment created. Starting virtual environment..."
 source .venv/bin/activate
 echo "Installing required packages..."
-pip install py-cord
-pip install xlsxwriter
-pip install xlwings
-pip install openpyxl
-pip install python-dotenv
-pip install xlwings
-pip install prompt_toolkit
-pip install requests
+pip install -r requirements.txt
 echo "All packages installed. Deactivating the virtual environment..."
 deactivate
